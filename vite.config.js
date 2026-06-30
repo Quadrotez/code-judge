2dce1c8e45fbac68b8b50d74f2e18226568e08f9
Your Name <you@example.com>
2026-06-30 08:27:35 +0300

    feat: updated base path in vite config because it works now on new domain

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/code-judge/',
+  base: '/',  // ← Измени это
 
   server: {
     port: 5173,
-
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
